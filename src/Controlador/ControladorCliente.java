8237145fcbe5c4742d3a51b4d79c7938f6bb36a5
Lorenzo R <lorenzortrujillo@outlook.com>
2017-11-10 16:13:16 -0600

    Comentario

--- src/Controlador/ControladorCliente.java
+++ src/Controlador/ControladorCliente.java
@@ -1,7 +1,7 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor. Comentario by Lorenzo
  */
 package Controlador;
 import Modelo.ClienteDAO;
